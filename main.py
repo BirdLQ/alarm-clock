a0623b736928131cd810ac47f31f567e0c12eacf
BirdLQ <toutlemonder@gmail.com>
2020-10-08 08:50:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-import sys, webbrowser, time
+# -*- coding: utf-8 -*-
+
+import sys, webbrowser, time, winsound, os
+
 from tkinter import *
-from time import strftime
-import datetime
 from tkinter import messagebox
+
 from PIL import ImageTk, ImageDraw
 import PIL.Image
-from win10toast import ToastNotifier
 
+from win10toast import ToastNotifier
+from pygame import mixer
+
+#ini
 window = Tk()
+toaster = ToastNotifier()
+mixer.init()
+    
+#wdw
 window.title('Reveil')
 w = 550
 h = 400
@@ -19,164 +28,244 @@ window.geometry('%dx%d+%d+%d' % (w, h, x, y))
 window.resizable(False, False)
 (width, height) = (550, 400)
 
-"""toaster = ToastNotifier()
 
-display = Label(window, textvariable="", font=('calibri', 40, 'bold'), 
-            background = '#1b2836', 
-            foreground = '#fafafa')
-display.config(text="00:00:00")
-display.place(relx=0.5, rely=0.26, anchor=CENTER)
-state = False
+class simpleAnimation:
+    def __init__(self, animationOn):
+        self.animationOn = animationOn
+        self.size = 10
+        self.im = PIL.Image.new( "RGB", (400, 400), (255, 255, 255) )
+        self.photo = ImageTk.PhotoImage(self.im)
+        self.label = Label(window, image=self.photo)
+        self.label.image = self.photo
+        self.label.pack()
+        self.buttonHolder = Frame(window)
+        self.buttonHolder.pack()
+        self.animate()
 
-def countdown():
-    global state,hours,mins, secs
-    
-    if state == True:
+    def interpolate(self, color_a, color_b, t):
+        return tuple(int(a + (b - a) * t) for a, b in zip(color_a, color_b))
 
-        if hours == 0 and mins == 0 and secs == 0:
-            display.config(text="Done!",font = ('calibri', 40, 'bold'))
-            state = False
-            webbrowser.open(str(t3.get()))
-        else:
-            display.config(text="%02d:%02d:%02d" % (hours, mins, secs))
-            
-            if mins==0:
-                if hours!=0:
-                    hours-=1
-                    mins=59
-            if secs == 0:
-                mins -= 1
-                secs = 59
-            else:
-                secs -= 1
-
-
-            window.after(1000, countdown)
-            
-def start():
-    global state, hours, mins, secs
-    if state == False:
-        state = True
-        hours = int(hourEntry.get())
-        mins = int(minuteEntry.get())
-        secs = 0
-        if not str(t3.get()):
-            messagebox.showinfo("Time Countdown","Please input a valid link")
-        else:
-            countdown()
-            toaster.show_toast("Reveil", "Le décompte a commencé",
-                               threaded=True, icon_path=None, duration=5)
+    def Application(self, *args, **kwargs):
+        self.labelIntro = Label(window, text="CREATED BY\nEL HAMIDI OTHMANE", font=('calibri', 36, 'bold'),
+                            background = '#1b2836')
+        self.labelIntro.place(relx=0.515, rely=0.45, anchor=CENTER)
         
+        self.display_version = Label(window, text="VERSION: ALPHA (V2)", font=('calibri', 12, 'bold'), 
+                        background = '#1b2836', 
+                        foreground = '#fafafa')
+        self.display_version.place(relx=0.725, rely=0.94, anchor=NW)
 
-lbl1=Label(window, text='Heure:')
-lbl1.config(font=("calibri", 15, 'bold'), background = '#1b2836', foreground = '#fafafa')
+        self.label_background_8_bit_color = tuple((27, 40, 54))
 
-lbl2=Label(window, text='Minute:')
-lbl2.config(font=("calibri", 15, 'bold'), background = '#1b2836', foreground = '#fafafa')
+        self.label_foreground_8_bit_color = tuple((250, 250, 250))
 
-lbl3=Label(window, text='Lien:')
-lbl3.config(font=("calibri", 15, 'bold'), background = '#1b2836', foreground = '#fafafa')
+        self.start_color = self.label_background_8_bit_color
+        self.end_color = self.label_foreground_8_bit_color
 
-t3=Entry(bd=0)
-hourEntry= Entry(window,bd=0, width=10, font=("arial",10,""), 
-         textvariable="") 
-hourEntry.place(relx=0.48, rely=0.45, anchor=W)
+        self.duration_ms = 1000
+        self.frames_per_second = 60
+        self.ms_sleep_duration = 1500 // self.frames_per_second
+        self.current_step = 0
 
-minuteEntry= Entry(window,bd=0, width=10, font=("arial",10,""), 
-           textvariable="") 
-minuteEntry.place(relx=0.48, rely=0.55, anchor=W)
+        self.update_label()
+        
+    def update_label(self):
+        self.t = (1.0 / self.frames_per_second) * self.current_step
+        self.current_step += 1
 
-lbl1.place(relx=0.4, rely=0.45, anchor=CENTER)
+        self.new_color = self.interpolate(self.start_color, self.end_color, self.t)
+        self.labelIntro.configure(foreground="#%02x%02x%02x" % self.new_color)
 
-lbl2.place(relx=0.4, rely=0.55, anchor=CENTER)
+        if self.current_step <= self.frames_per_second:
+            window.after(self.ms_sleep_duration, self.update_label)
 
-lbl3.place(relx=0.4, rely=0.65, anchor=CENTER)
 
-t3.place(relx=0.48, rely=0.65, anchor=W)
+    def animate(self):
+        self.newIm = PIL.Image.new( "RGB", (width, height), (0, 0, 0) )
+        self.draw = ImageDraw.Draw(self.newIm)
+        (cX, cY) = (width/2, height/2)
+        self.draw.ellipse( [cX-self.size/3, cY-self.size/3, cX+self.size/3, cY+self.size/3], (27, 40, 54))
+        self.photo = ImageTk.PhotoImage(self.newIm)
+        self.label.configure(image = self.photo)
+        self.label.image = self.photo
+        if self.animationOn and self.size<=1100:
+            self.size += 15
+            self.label.after(10, self.animate)
+        else:
+            self.Application()
+            self.labelIntro.after(3100, self.labelIntro.pack)
 
-b1=Button(window, bd=0,relief="groove", text='Activer', font=('calibri',12, 'bold'), cursor='hand2', command=start)
-b1.place(relx=0.5, rely=0.85, anchor=CENTER)  
+simpleAnimation(True)
 
-def time(): 
-    string = strftime('%H:%M:%S %p') 
-    lbl.config(text = string) 
-    lbl.after(1000, time)
+def main():
+#    def atime(): 
+#        string = time.strftime('%H:%M:%S %p') 
+#        lbl.config(text = string) 
+#        lbl.after(1000, atime)
+#        
+#    lbl = Label(window, font = ('calibri', 40, 'bold'), 
+#                background = '#1b2836', 
+#                foreground = '#fafafa', anchor=N)
+#    lbl.pack()
+#    
+#    atime()
+  
+    global state, display
     
-lbl = Label(window, font = ('calibri', 40, 'bold'), 
-            background = '#1b2836', 
-            foreground = '#fafafa', anchor=N)
-lbl.pack()
+    display = Label(window, textvariable="", font=('calibri', 52, 'bold'), 
+                background = '#1b2836', 
+                foreground = '#fafafa')
+    display.config(text="00:00:00")
+    display.place(relx=0.5, rely=0.22, anchor=CENTER)
+    display.pack
+    state = False
 
-time()"""
+    def countdown():
+        global state,hours,mins, secs
+        
+        if state == True:
+            if hours == 0 and mins == 0 and secs == 0:
+                display.config(text="Done!",font = ('calibri', 52, 'bold'))
+                state = False
+                if str(link.get()) == "www.exemple.com":
+                    mixer.music.load("music")
+                    mixer.music.play()
+                else:
+                    webbrowser.open(str(link.get()))
+            else:
+                display.config(text="%02d:%02d:%02d" % (hours, mins, secs))
+                
+                if mins==0:
+                    if hours!=0:
+                        hours-=1
+                        mins=59
+                if secs == 0:
+                    mins -= 1
+                    secs = 59
+                else:
+                    secs -= 1
 
-size = 10
-animationOn = False
 
-def interpolate(color_a, color_b, t):
-    return tuple(int(a + (b - a) * t) for a, b in zip(color_a, color_b))
+                window.after(1000, countdown)
+                
+    def start():
+        global state, hours, mins, secs, btn_state
+        
+        if state == False:
+            state = True
+            
+            if not str(hourEntry.get()):
+                hours = 0
+            else:
+                hours = int(hourEntry.get())
+                
+            if not str(minuteEntry.get()):
+                mins = 0
+            else:
+                mins = int(minuteEntry.get())
 
-def Application(*args, **kwargs):
-    global frames_per_second, current_step, start_color, end_color, labelIntro, ms_sleep_duration
+            secs = 0
+            
+            if hours==0 and mins==0:
+                messagebox.showinfo("Time Countdown","Saisie invalide")
+                               
+            else:
+                countdown()
 
-    labelIntro = Label(window, text="CREATED BY\nBIRD LQ", font=('calibri', 36, 'bold'),
-                        background = '#1b2836')
-    labelIntro.place(relx=0.515, rely=0.45, anchor=CENTER)
+                lbl1.destroy()
+                lbl2.destroy()
+                lbl3.destroy()
+                link.destroy()
+                hourEntry.destroy()
+                minuteEntry.destroy()
+                
+                display.place(y=display.winfo_y()+50)
+                display.config(font = ('calibri', 92, 'bold'))
+                    
+                toaster.show_toast("Reveil", "Le décompte a commencé",
+                                   threaded=True, icon_path=None, duration=5)
+
+    lbl1=Label(window, text='Heure:')
+    lbl1.config(font=("calibri", 15, 'bold'), background = '#1b2836', foreground = '#fafafa')
     
-    display_version = Label(window, text="VERSION: ALPHA (V2)", font=('calibri', 12, 'bold'), 
-                    background = '#1b2836', 
-                    foreground = '#fafafa')
-    display_version.place(relx=0.725, rely=0.94, anchor=NW)
-
-    start_color = tuple((27, 40, 54))
-    end_color = tuple((250, 250, 250))
-
-    duration_ms = 1000
-    frames_per_second = 60
-    ms_sleep_duration = 1600 // frames_per_second
-    current_step = 0
-
-    update_label()
+    lbl2=Label(window, text='Minute:')
+    lbl2.config(font=("calibri", 15, 'bold'), background = '#1b2836', foreground = '#fafafa')
     
-def update_label():
-    global frames_per_second, current_step
+    lbl3=Label(window, text='Lien:')
+    lbl3.config(font=("calibri", 15, 'bold'), background = '#1b2836', foreground = '#fafafa')
+    
+    link=Entry(window,bd=0, font=("arial",10,""), foreground = 'grey', 
+               textvariable="")
+    link.insert(0,"www.exemple.com")
+    link.place(relx=0.48, rely=0.65, anchor=W)
+    
+    hourEntry= Entry(window, bd=0, width=10, font=("arial",10,""), foreground = 'grey', 
+             textvariable="")
+    hourEntry.insert(0,"00")
+    hourEntry.place(relx=0.48, rely=0.45, anchor=W) 
+    
+    minuteEntry= Entry(window,bd=0, width=10, font=("arial",10,""), foreground = 'grey', 
+               textvariable="")
+    minuteEntry.insert(0,"00")
+    minuteEntry.place(relx=0.48, rely=0.55, anchor=W)
+    
+    def supp(event):
+        if str(link.get())=="":
+            link.insert(0,"www.exemple.com")
+        try:
+            if int(hourEntry.get())<=1:
+                event.widget.delete(0,"end")
+        except:
+            hourEntry.insert(0,"00")
+        try:  
+            if int(minuteEntry.get())<=1:
+                event.widget.delete(0,"end")
+        except:
+            minuteEntry.insert(0,"00") 
+        if str(link.get()) == "www.exemple.com":
+            event.widget.delete(0,"end")
+        return None
 
-    t = (1.0 / frames_per_second) * current_step
-    current_step += 1
+    hourEntry.bind("<Button-1>",supp)
+    minuteEntry.bind("<Button-1>",supp)
+    link.bind("<Button-1>",supp)
+    
+    lbl1.place(relx=0.4, rely=0.45, anchor=CENTER) 
+    lbl2.place(relx=0.4, rely=0.55, anchor=CENTER) 
+    lbl3.place(relx=0.4, rely=0.65, anchor=CENTER) 
+    
+    def switch_button():
+        global btn_state, win_btn, off_btn
+        btn_state=1
 
-    new_color = interpolate(start_color, end_color, t)
-    labelIntro.configure(foreground="#%02x%02x%02x" % new_color)
+        def test(event):
+            global btn_state
+            if btn_state == 1:
+                #win_btn.config(image=mid_btn)
+                #window.after(100,None)
+                win_btn.config(image=on_btn)
+                start()
+                btn_state=0
+            else:
+                #win_btn.config(image=mid_btn)
+                #window.after(100,None)
+                win_btn.config(image=off_btn)
+                main()
+                btn_state=1
 
-    if current_step <= frames_per_second:
-        window.after(ms_sleep_duration, update_label)
+        on_btn=PhotoImage(file='on.png')
+        #mid_btn=PhotoImage(file='mid.png')
+        off_btn=PhotoImage(file='off.png')
 
-def simpleAnimation():
-    global label, height, width, buttonHolder
-    im = PIL.Image.new( "RGB", (400, 400), (255, 255, 255) )
-    photo = ImageTk.PhotoImage(im)
-    label = Label(window, image=photo)
-    label.image = photo
-    label.pack()
-    buttonHolder = Frame(window)
-    buttonHolder.pack()
-    animate()
+        win_btn = Label(window, image=off_btn, cursor="hand2")
+        win_btn.place(relx=0.5, rely=0.85, anchor=CENTER)
+        win_btn.config(bg='#1b2836')
+        win_btn.bind("<Button-1>",test)
 
-def animate():
-    global size, label, width, height, animationOn, intro
-    newIm = PIL.Image.new( "RGB", (width, height), (0, 0, 0) )
-    draw = ImageDraw.Draw(newIm)
-    (cX, cY) = (width/2, height/2)
-    draw.ellipse( [cX-size/3, cY-size/3, cX+size/3, cY+size/3], (27, 40, 54))
-    photo = ImageTk.PhotoImage(newIm)
-    label.configure(image = photo)
-    label.image = photo
-    if animationOn and size<=1100:
-        size += 15
-        label.after(10, animate)
-    else:
-        Application()
-        labelIntro.after(3100, labelIntro.pack)
+        
+    switch_button()
 
-animationOn = True
-simpleAnimation()
+
+window.after(4200,main)
 
 window.mainloop()
